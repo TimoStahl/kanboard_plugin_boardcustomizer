d5e6853f4b9bfa51512c61c3856bf3944a873959
BlueTeck <tili2@gmx.de>
2019-07-06 18:14:54 +0200

    [INTERNAL] cleanup

--- Controller/SettingsController.php
+++ Controller/SettingsController.php
@@ -12,28 +12,28 @@ class SettingsController extends BaseController
         $user = $this->getUser();
 
         $options = [
-            'Board: only show first column header' => 'boardcustomizer_onlyfirstcolumnheaders',
-            'Board: decent hidden column' => 'boardcustomizer_optimizehiddencolumn',
-            'Board: top selection without scollbar' => 'boardcustomizer_topnavhiddenscrollbar',
-            'Card: material design' => 'boardcustomizer_materialcard',
-            'Card: hide task priority' => 'boardcustomizer_hidetaskpriority',
-            'Card: hide task age' => 'boardcustomizer_hidetaskage',
-            'Card: white background' => 'boardcustomizer_whitebackground'
+            t('Board: only show first column header') => 'boardcustomizer_onlyfirstcolumnheaders',
+            t('Board: decent hidden column') => 'boardcustomizer_optimizehiddencolumn',
+            t('Board: top selection without scollbar') => 'boardcustomizer_topnavhiddenscrollbar',
+            t('Card: material design') => 'boardcustomizer_materialcard',
+            t('Card: hide task priority') => 'boardcustomizer_hidetaskpriority',
+            t('Card: hide task age') => 'boardcustomizer_hidetaskage',
+            t('Card: white background') => 'boardcustomizer_whitebackground'
         ];
 
         // additional options is other plugin is installed
         $pluginFGroupAssign = PLUGINS_DIR . DIRECTORY_SEPARATOR . basename('Group_assign');
         if (file_exists($pluginFGroupAssign)) {
             $plugin_groupassign = [
-                'Card: hide group labels' => 'boardcustomizer_groupassign_hidecardlabels'
+                t('Card: hide group labels') => 'boardcustomizer_groupassign_hidecardlabels'
             ];
             $options = array_merge($options, $plugin_groupassign);
         }
 
         $this->response->html($this->helper->layout->user('boardcustomizer:user/settings', [
-            'title' => t('Display settings'),
-            'user'                                                                   => $user,
-            'options'                                                               => $options,
+            'title'   => t('My display settings'),
+            'user'    => $user,
+            'options' => $options,
         ]));
     }
 
@@ -49,7 +49,6 @@ class SettingsController extends BaseController
     {
         $user = $this->getUser();
         $key = $this->request->getStringParam('key');
-
         $this->userMetadataModel->save($user['id'], [$key => true]);
         return $this->response->redirect($this->helper->url->to('SettingsController', 'showSettings', ['plugin' => 'boardcustomizer']), true);
     }
