297e83664468ab45c81a8478ead2b90d53b55009
Jieiku <106644+Jieiku@users.noreply.github.com>
2021-12-22 14:31:41 -0800

    Compact Layout

---
+++ Template/board/task_public.php
@@ -0,0 +1,46 @@
+<div class="task-board color-<?= $task['color_id'] ?> <?= $task['date_modification'] > time() - $board_highlight_period ? 'task-board-recent' : '' ?>">
+    <div class="task-board-header">
+        <?= $this->url->link('#'.$task['id'], 'TaskViewController', 'readonly', array('task_id' => $task['id'], 'token' => $project['token'])) ?>
+
+        <?php if (! empty($task['owner_id'])): ?>
+            <span class="task-board-assignee">
+                <?= $this->text->e($task['assignee_name'] ?: $task['assignee_username']) ?>
+            </span>
+        <?php endif ?>
+
+        <div class="task-board-avatars-outer">
+            <div class="task-board-icons-top">
+                <div class="task-board-icons-abs-topright">
+                    <?php if ($this->user->userMetadataModel->exists($this->user->getid(), "boardcustomizer_compactlayout")) { ?>
+                        <?= $this->render('board/task_footer', array(
+                            'task' => $task,
+                            'not_editable' => $not_editable,
+                            'project' => $project,
+                        )) ?>
+                    <?php } ?>
+                </div>
+            </div>
+            <div class="task-board-avatars-inner">
+                <?= $this->hook->render('template:board:public:task:before-avatar', array('task' => $task)) ?>
+                <?= $this->render('board/task_avatar', array('task' => $task)) ?>
+                <?= $this->hook->render('template:board:public:task:after-avatar', array('task' => $task)) ?>
+            </div>
+        </div>
+    </div>
+
+    <?= $this->hook->render('template:board:public:task:before-title', array('task' => $task)) ?>
+    <div class="task-board-title">
+        <?= $this->url->link($this->text->e($task['title']), 'TaskViewController', 'readonly', array('task_id' => $task['id'], 'token' => $project['token'])) ?>
+    </div>
+    <?= $this->hook->render('template:board:public:task:after-title', array('task' => $task)) ?>
+
+    <div class="task-board-icons-bottom">
+        <?php if (!$this->user->userMetadataModel->exists($this->user->getid(), "boardcustomizer_compactlayout")) { ?>
+            <?= $this->render('board/task_footer', array(
+                'task' => $task,
+                'not_editable' => $not_editable,
+                'project' => $project,
+            )) ?>
+        <?php } ?>
+    </div>
+</div>
